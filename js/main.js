4c89b8f36722830abad20d57beec90d2540b94af
Standaman1 <stanleyshrink@hotmail.com>
2022-01-10 17:10:05 +1100

    draft

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$("button").click(function(){
+    $(this).css("background-color", "blue");
+}) //change value of square
+
+
+// 1. Intitiate Game
+// 2. Player select icon on each side - put icon in icon window
+// 3. Randomise who goes first
+// 4. Button function for selection
+// 5. Check if square has been selected
+// 6. If so enact change and prompt next player
+// 7. TTT logic and check the parameter changed
+// 8. Winner with celebration animation and update score and play game again. 
+
+
