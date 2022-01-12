7f377fd2562e89d48a645ab61bd25400874404dc
Standaman1 <stanleyshrink@hotmail.com>
2022-01-12 11:08:51 +1100

    wedmorningcommit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,143 @@
-$("button").click(function(){
-    $(this).css("background-color", "blue");
-}) //change value of square
 
 
-// 1. Intitiate Game
-// 2. Player select icon on each side - put icon in icon window
-// 3. Randomise who goes first
-// 4. Button function for selection
-// 5. Check if square has been selected
-// 6. If so enact change and prompt next player
-// 7. TTT logic and check the parameter changed
-// 8. Winner with celebration animation and update score and play game again. 
+
+
+
+
+// const checkWin = function(){
+//     for (let i=0; i<3; i++){ //or i=board size
+//         let rowCheck = $(".tttsquare").attr("data-row")
+//         if($(".tttsquare").attr("data-row")){
+
+
+
+
+
+
+//         }
+//         //if id of row/column is the same
+//         //if diagonal win condition id is same
+
+
+
+//     }
+// }
+$(document).ready(function(){
+
+// Create board function
+const boardSize = 3;
+let initialMatrix = [[boardSize],[boardSize]];
+
+
+
+
+// let createBoard = function(boardSize){
+
+    
+//     for (let i=0; i<boardSize; i++){
+//         let $newSquare = $("<div>");
+
+//         for (let j=0; j<boardSize ; j++){
+//             //add square
+//             $($newSquare, {
+//                 id: `data-row=${i}, data-column=${j}`,
+
+//             }).appendTo(".board")
+            
+//             // $(".board").append("<div></div>");
+//             // let newButton = $('<input type="button"/>');
+//             // //add div to column 
+//             // $(".board").append($newSquareNew);
+//             // console.log(newSquare);
+//         }
+//     }
+
+// } //createsBoard
+// createBoard(3)
+
+// Create Board Function Ends--------------------
+
+
+
+let playerTurn = 1;
+
+let dataRow = parseInt($(".tttsquare").data("row"));
+let dataColumn = parseInt($(".tttsquare").data("column"));
+
+
+
+console.log(dataColumn, dataRow)
+
+
+
+
+const rowCheck = function(k,l){
+
+        
+    for (let i=0;i<3;i++)
+    {
+        for (let j=0;j<3;j++)  
+        {
+            let newinitialMatrix = initialMatrix[i][j];
+
+            console.log(newinitialMatrix)
+            newinitialMatrix[k][l] = playerTurn;
+            if (initialMatrix[1][l] || (initialMatrix[i][1]))
+                
+                { 
+                    console.log("Player" + playerTurn)
+                }
+        }
+    }    
+}
+//check if won 
+
+
+$(function(){
+
+    $(".tttsquare").click(function(){
+        if ($(this).attr("class") === "tttsquare Player1" || $(this).attr("class") === "tttsquare Player2"){
+        console.log("Try Again")
+
+        } else if (playerTurn == 1){
+            $(this).addClass("Player1")
+            // css("background-color", "purple");
+            //change this to change value instead of color
+            //put indication it is P2
+
+            rowCheck(dataRow,dataColumn)
+            playerTurn = 2;
+            
+        } else {
+            $(this).addClass("Player2")
+            playerTurn = 1;
+
+        }
+
+    }) //adds ID and changes turn. 
+})
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
