266b40ef8f589f1b751b1cdf9052da05f3c17cb6
Standaman1 <stanleyshrink@hotmail.com>
2022-01-14 19:58:21 +1100

    fridaycode

--- js/main.js
+++ js/main.js
@@ -1,40 +1,14 @@
 
 
-// const boardSize = 3; //set to dropdown selection
-
-
-
-// $(".board").html("").css("grid-template-columns", `repeat(3, 1fr)`) //get board size
-
-
-// const createBoard = function(){
-//     for (let i=0; i<3; i++){
-       
-//         for (let j=0; j<3; j++){
-//             //add square
-//             let $newSquare = $("<div>", {
-//                 "class": "boardsquare",
-//             })
-//             $newSquare.data("row", `${i}`)
-//             $newSquare.data("column", `${j}`)
-//             $(".board").append($newSquare)
-
-//             let squareRow = parseInt($(this).data("row"));
-//             let sqaureColumn = parseInt($(this).data("column"));
-//             // $(".board").append("<div></div>");
-//             // let newButton = $('<input type="button"/>');
-//             // //add div to column 
-//             // $(".board").append($newSquareNew);
-//             // console.log(newSquare);
-//         }
-    
-
-// }}//createsBoard
-
-
-
 $(document).ready(function(){
 
+let playerTurn = 1;
+let Player1Icon
+let Player2Icon
+let drawCounter = 0;
+let gameOver = false;
+let P1Score = 0;
+let P2Score = 0;
 
 // Create board function
 const boardSize = 3;
@@ -42,14 +16,14 @@ let initialMatrix = Array(boardSize);
 
 const createMatrix = function(){
     
-    for (let m=0; m<boardSize; m++)
-    {
+    for (let m=0; m<boardSize; m++){
+        
         initialMatrix[m] = []
-        // console.log(initialMatrix)
-        for (let n=0; n<boardSize; n++)
-        {
-            initialMatrix[m][n] = 0;
-        }
+
+            for (let n=0; n<boardSize; n++)
+            {
+                initialMatrix[m][n] = 0;
+            }
     }
     
     return initialMatrix;
@@ -57,219 +31,132 @@ const createMatrix = function(){
 
 createMatrix()
 
-
-// console.log(initialMatrix)
-
-// let createBoard = function(boardSize){
-
-    
-//     for (let i=0; i<boardSize; i++){
-//         let $newSquare = $("<div>");
-
-//         for (let j=0; j<boardSize ; j++){
-//             //add square
-//             $($newSquare, {
-//                 id: `data-row=${i}, data-column=${j}`,
-
-//             }).appendTo(".board")
-            
-//             // $(".board").append("<div></div>");
-//             // let newButton = $('<input type="button"/>');
-//             // //add div to column 
-//             // $(".board").append($newSquareNew);
-//             // console.log(newSquare);
-//         }
-//     }
-
-// } //createsBoard
-// createBoard(3)
-
-// Create Board Function Ends--------------------
-
-
-
-let playerTurn = 1;
-
-
-
-
-//check if it has a class or symbol
-// return initial matrix 
-
-
-
-
-// const columnCheck = function(){
-//     for (let j=0;j<3;j++) {
-//         if ((initialMatrix[0][j] === initialMatrix[1][j]) && (initialMatrix[1][j] === initialMatrix[2][j]) && (initialMatrix[0][j] != 0))
-        
-//         {
-//             console.log(`"Player" + "${initialMatrix[0][j]}"`)
-
-//         }
-//     }
-
-// }
-
-// const rowCheck = function(){
-//     for (let i=0;i<3;i++)
-
-//     { if((initialMatrix[i][0] === initialMatrix[i][1]) && (initialMatrix[i][1]=== initialMatrix[i][2]) && (initialMatrix[i][0] != 0))
-//         {
-//             console.log(`"Player" + "${initialMatrix[i][0]}"`)
-
-//         }
-//     }
-// }
-
-
-// const diagonalCheck = function(){
-//     if(initialMatrix[0][0] == initialMatrix[1][1] && initialMatrix[1][1] == initialMatrix[2][2] && initialMatrix[0][0] != 0) {
-//         {
-//             console.log(`"Player" + "${initialMatrix[0][0]}"`)
-
-//         }
-// }}
-
-
 const rowCheck = function(){
 
-    for (let i=0;i<3;i++)
-    
-    {   let P2Counter = 0;
-        let P1Counter = 0;
+    for (let i=0;i<3;i++){   
         
-    for (let j=0;j<3;j++) {
-    
-    if (initialMatrix[i][j] != 0){
-        if (initialMatrix[i][j] != 1){
-        P2Counter ++
-        // console.log("P2Counter", P2Counter)
+        let P2Counter = 0;
+        let P1Counter = 0;
+            
+        for (let j=0;j<3;j++){
+        
+            if (initialMatrix[i][j] != 0){
 
-        } else {
-        P1Counter ++
-        // console.log("P1Counter", P1Counter)
+                if (initialMatrix[i][j] != 1){
+                    P2Counter ++
+
+                } else {
+                    P1Counter ++
+
+                }
+
+                if (P2Counter == 3){
+                    console.log("P2WinsR")
+                    P2Score++
+                    $(".tttSquare").off("click")
+                    $("#P2WinMessage").show()
+                    gameOver = true;
+                    return
+
+                } else if (P1Counter == 3){
+                    console.log("P1WinsR")
+                    P1Score++
+                    $(".tttSquare").off("click")
+                    $("#P1WinMessage").show()
+                    gameOver = true;
+                    return
+
+                }
+            }    
         }
-        if (P2Counter == 3){
-            console.log("P2WinsR")
-            P2Score++
-            $(".tttSquare").off("click")
-
-            $("#P2WinMessage").show()
-            gameOver = true;
-            return
-        }
-         
-        else if(P1Counter == 3){
-            console.log("P1WinsR")
-            P1Score++
-            $(".tttSquare").off("click")
-
-            $("#P1WinMessage").show()
-            gameOver = true;
-
-            return
-
-        }
-}    
-}
-}}
-
-
+    }
+} //checks rowena
 
 
 const columnCheck = function(){
 
-    for (let j=0;j<3;j++)
-    
-    {   let P2Counter = 0;
+    for (let j=0;j<3;j++){   
+        
+        let P2Counter = 0;
         let P1Counter = 0;
         
-    for (let i=0;i<3;i++) {
-    
-    if (initialMatrix[i][j] != 0){
-        if (initialMatrix[i][j] != 1){
-        P2Counter ++
-        // console.log("P2Counter", P2Counter)
-
-        } else {
-        P1Counter ++
-        // console.log("P1Counter", P1Counter)
-        }
-        if (P2Counter == 3){
-            console.log("P2WinsC")
-            P2Score++
-            $(".tttSquare").off("click")
+            for (let i=0;i<3;i++) {
             
-            $("#P2WinMessage").show()
-            gameOver = true;
+            if (initialMatrix[i][j] != 0){
 
-            return
+                if (initialMatrix[i][j] != 1){
+                P2Counter ++
+        
 
+                } else {
+                P1Counter ++
+
+                }
+
+                if (P2Counter == 3){
+                    console.log("P2WinsC")
+                    P2Score++
+                    $(".tttSquare").off("click")
+                    $("#P2WinMessage").show()
+                    gameOver = true;
+                    return
+
+                } else if(P1Counter == 3){
+                    console.log("P1WinsC")
+                    P1Score++
+                    $(".tttSquare").off("click")
+                    
+                    $("#P1WinMessage").show()
+                    gameOver = true;
+
+                    return
+
+
+                }
+            }    
         }
-         
-        else if(P1Counter == 3){
-            console.log("P1WinsC")
-            P1Score++
-            $(".tttSquare").off("click")
-            
-            $("#P1WinMessage").show()
-            gameOver = true;
-
-            return
-
-
-        }
-}    
-}
-}}
-
+    }
+} //check column winner
 
 
 const diagonalCheck = function(){
+
     let P1Counter = 0;
     let P2Counter = 0;
-    for (let i=0;i<3;i++)
 
-        {   
-            if (initialMatrix[i][i] != 0){
+    for (let i=0;i<3;i++){
+
+        if (initialMatrix[i][i] != 0){
 
             if (initialMatrix[i][i] != 1){
                 P2Counter ++
-                // console.log("P2Counter", P2Counter)
             }
-                else {
-                P1Counter ++
-                // console.log("P1Counter", P1Counter)
 
+            else {
+                P1Counter ++
                 }
+
             if (P2Counter == 3){
                 console.log("P2WinsD")
                 P2Score++
                 $(".tttSquare").off("click")
-
                 $("#P2WinMessage").show()
                 gameOver = true;
-
                 return
 
                 }
                 
-            if(P1Counter == 3){
+            if (P1Counter == 3){
                 console.log("P1WinsD")
                 P1Score++
                 $(".tttSquare").off("click")
-
                 $("#P1WinMessage").show()
                 gameOver = true;
-
                 return
-                    
-        
-                }
-
-}
-}}
+            }
+        }
+    }
+} //checks diagonal winner
 
 const diagonalCheckReverse = function(){
     let P1Counter = 0;
@@ -281,10 +168,10 @@ const diagonalCheckReverse = function(){
 
             if (initialMatrix[i][j] != 1){
                 P2Counter ++
-                // console.log("P2Counter", P2Counter)
+
             } else {
                 P1Counter ++
-                //   console.log("P1Counter", P1Counter)
+
             }
             if (P2Counter == 3){
                     console.log("P2WinsDiag")
@@ -299,7 +186,7 @@ const diagonalCheckReverse = function(){
             if(P1Counter == 3){
                     console.log("P1WinsDiag")
                     P1Score++
-            gameOver = true;
+                    gameOver = true;
                     
 
                     $("#P1WinMessage").show()
@@ -308,70 +195,28 @@ const diagonalCheckReverse = function(){
                 }
 
 }
-}}
+}} //checks other diagonal winner
 
-// const columnCheck = function(){
-    
-//     for (let j=0;j<3;j++)
-    
-//     {
-        
-//     for (let i=0;i<3;i++) 
-    
-//     {
 
-//         if (initialMatrix[i][j] != 0 && (initialMatrix[i][j] != 1)){
-//             console.log("P2Wins")
-//             // return Player2Wins; return something else
+const drawCheck = function(){
+    if (drawCounter === 9){
+        console.log("Draw")
+        $("#DrawMessage").show()
+    };
 
-//         } else if (initialMatrix[i][j] != 0 && (initialMatrix[i][j] != 2)){
-//             console.log("P1Wins")
-//             // return Player1Wins;
-//         }
-        
-    
-//     }    
-// } 
-// // console.log(initialMatrix)
-// }
+} //checks for the draw
 
-// const diagonalCheck = function(){
 
-//     for (let i=0;i<3;i++)
-//         {
+const winCondition = function(){
+    rowCheck()
+    columnCheck()
+    diagonalCheck()
+    diagonalCheckReverse()
+    drawCheck()
 
-//         if (initialMatrix[i][i] != 0 && (initialMatrix[i][i] != 1))
-//         {
-//             console.log("P2Wins")
-            
+} //all the win condition functions
 
-//         } else if (initialMatrix[i][i] != 0 && (initialMatrix[i][i] != 2)
-//         ){
-//             console.log("P1Wins")
-            
-    
-//         } else 
-        
-//             for (let j=3;j<0;j--) 
-    
-//                 {
-
-//                     if (initialMatrix[i][j] != 0 && (initialMatrix[i][j] != 1)){
-//                         console.log("P2Wins")
-                        
-            
-//                     } else if (initialMatrix[i][j] != 0 && (initialMatrix[i][j] != 2)){
-//                         console.log("P1Wins")
-
-        
-//                     }
-//                 }    
-// }
-// }
-
-let Player1Icon
-let Player2Icon
-
+// Changes icon to selection -------------
 $("#pink").on("click", function(){
     Player1Icon = "pink"
 })
@@ -387,131 +232,69 @@ $("#yellow").on("click", function(){
 $("#lightseagreen").on("click", function(){
     Player2Icon = "lightseagreen"
 })
+// end icon selection change ----------
+
+$(".tttsquare").click(function(){
+
+    let dataRow = parseInt($(this).data("row"));
+    let dataColumn = parseInt($(this).data("column"));
 
 
-let drawCounter = 0;
-const drawCheck = function(){
-    if (drawCounter === 9){
-        console.log("Draw")
-        $("#DrawMessage").show()
-    };
+    if ($(this).hasClass("clicked") || gameOver){
+        return;
 
-    // $(".tttsquare").each(function(){
-    //     if (!$(this).hasClass("clicked") === 0){
-    //         console.log("Draw")
-    //     }
-
-    // })
-
-}
-    // for (i=0;i<3;i++){
-
-    //     for (j=0;j<3;j++){
-        // if($(".tttSqaure").each.eq(i).data("row").hasClass("clicked") && $(".tttSqaure").each.eq(j).data("column").hasClass("clicked")) {
-        //     console.log("Draw")
-        // } 
-
-//         }
-
-        
-//     }
-// }
-let gameOver = false;
-
-const winCondition = function(){
-    rowCheck()
-    columnCheck()
-    diagonalCheck()
-    diagonalCheckReverse()
-    drawCheck()
-
-}
-
-
-
-    $(".tttsquare").click(function(){
-   
-        let dataRow = parseInt($(this).data("row"));
-        let dataColumn = parseInt($(this).data("column"));
-        // console.log(dataRow)
-        // console.log(dataColumn)
-
- 
-        if ($(this).hasClass("clicked") || gameOver){
-            return;
-
-        } 
-        
-        if (drawCounter === 9  ){
-            console.log("ANything")
-            $(".tttSquare").off("click")
-        
-        }
-        
-        else if (playerTurn == 1){
- 
-        
-            $(this).addClass("clicked")
-            drawCounter++
-            console.log(drawCounter)
-            $(this).css("background-color", Player1Icon)
-            //$(this).attr() = `<i class="fa fa-home"></i>`
-            // css("background-color", "purple");
-            //change this to change value instead of color
-            //put indication it is P2
-            initialMatrix[dataColumn][dataRow] = 1;
-            playerTurn = 2;
-
-            
-            
-            
-            
-        } else {
+    } 
     
-            $(this).addClass("clicked")
-            $(this).css("background-color", Player2Icon)
-            drawCounter++
-            console.log(drawCounter)
-
-
-            initialMatrix[dataColumn][dataRow] = 2;
-            playerTurn = 1;
-
-             //change to symbol
-            
+    else if (drawCounter === 9 ){
+        console.log("ANything")
+        $(".tttSquare").off("click")
     
-
-        }
-
-        winCondition()
-        // if (drawCounter === 9 || $("#P1WinMessage").show()){
-        //     $(".tttSquare").off("click")
-        
-        // }
-        
     }
     
-    ) //adds ID and changes turn. 
+    else if (playerTurn == 1){
 
-    let P1Score = 0;
-    let P2Score = 0;
-    
-    
-    $(".replaybutton").on('click', function(){
-        $(".tttsquare").css("background-color", "purple")
-        createMatrix()
-        drawCounter=0;
-        gameOver = false;
-        $(".tttsquare").removeClass("clicked")
-        $("#P1Score").html(P1Score)
-        $("#P1WinMessage").hide()
-        $("#P2Score").html(P2Score)
-        $("#P2WinMessage").hide()
-        $("#DrawMessage").hide()
+        $(this).addClass("clicked")
+        drawCounter++
+        console.log(drawCounter)
+        $(this).css("background-color", Player1Icon)
+        //$(this).attr() = `<i class="fa fa-home"></i>`
+        initialMatrix[dataColumn][dataRow] = 1;
+        playerTurn = 2;
+        
+    } else {
+
+        $(this).addClass("clicked")
+        $(this).css("background-color", Player2Icon)
+        drawCounter++
+        console.log(drawCounter)
+        initialMatrix[dataColumn][dataRow] = 2;
         playerTurn = 1;
 
+    }
+
+    winCondition()
     
-    })
+}
+
+) //adds ID and changes turn. 
+
+
+$(".replaybutton").on('click', function(){
+
+    $(".tttsquare").css("background-color", "purple")
+    createMatrix()
+    drawCounter=0;
+    gameOver = false;
+    $(".tttsquare").removeClass("clicked")
+    $("#P1Score").html(P1Score)
+    $("#P1WinMessage").hide()
+    $("#P2Score").html(P2Score)
+    $("#P2WinMessage").hide()
+    $("#DrawMessage").hide()
+    playerTurn = 1;
+
+
+}) //reset variables and update score and reveal messages to play again
 
 
 });
